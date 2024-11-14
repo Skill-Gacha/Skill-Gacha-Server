63ced3e76f47117c98b665649f8fdf281c460053
Ayumudayo <yumemiayumu@gmail.com>
2024-11-14 18:28:54 +0900

    🐛 bugfix: DB 이름 잘못됐던 것 수정

--- src/db/user/user.queries.js
+++ src/db/user/user.queries.js
@@ -1,9 +1,8 @@
 // src/db/user/user.queries.js
 
 export const USER_QUERIES = {
-  FIND_USER_BY_NICKNAME: 'SELECT * FROM users WHERE nickname = ?',
+  FIND_USER_BY_NICKNAME: 'SELECT * FROM CharacterInfo WHERE nickname = ?',
   CREATE_USER:
-    'INSERT INTO users (nickname, job, level, maxHp, maxMp, atk, def, magic, speed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
-  UPDATE_USER_LOGIN: 'UPDATE users SET updateAt = CURRENT_TIMESTAMP WHERE username = ?',
-  FIND_HIGH_SCORE: 'SELECT MAX(highScore) AS highScore FROM users',
+    'INSERT INTO CharacterInfo (nickname, job, level, maxHp, maxMp, atk, def, magic, speed) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)',
+  UPDATE_USER_LOGIN: 'UPDATE CharacterInfo SET updateAt = CURRENT_TIMESTAMP WHERE username = ?',
 };
