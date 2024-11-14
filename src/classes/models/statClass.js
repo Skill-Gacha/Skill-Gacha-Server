8e0d2ab01f233135d4125803e949345c17d21367
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-14 21:30:35 +0900

    🩹 fix : 코드 수정

--- src/classes/models/statClass.js
+++ src/classes/models/statClass.js
@@ -1,12 +1,11 @@
 ﻿// src/classes/models/userClass.js
 
 class Stat {
-  
   constructor() {
     this.level = 0;
-    this.float = 0;
     this.hp = 0;
     this.maxHp = 0;
+    this.maxMp = 0;
     this.atk = 0;
     this.def = 0;
     this.magic = 0;
