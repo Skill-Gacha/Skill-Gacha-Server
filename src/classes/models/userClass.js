5b3cfcac316644bf577e4b01afafcb5118d5cb75
yoonyeobin <ugt9076@naver.com>
2024-11-14 16:22:29 +0900

    🎉 add: sMove핸들러 추가
    sanimationHandler.js 파일 추가
    
    [userClass.js]
    updateAnimCode 메서드 추가

--- src/classes/models/userClass.js
+++ src/classes/models/userClass.js
@@ -9,19 +9,23 @@ class User {
     this.socket = socket;
     this.id = id;
     this.nickname = nickname;
-    
+
     // 클래스 정보
     // 속성 구분을 위해 사용
     this.job = 0;
-    
+
     // 이모트용 감정표현 코드
     this.animCode = 0;
-    
+
     // 위치 정보
     this.position = new Position();
-    
+
     // 스탯 정보
     this.stat = new Stat();
+  }
+
+  updateAnimCode(animCode) {
+    this.animCode = animCode;
   }
 }
 
