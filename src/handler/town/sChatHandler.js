6557042dc57e60bcb2bf05bb5f60cf36fa6da34d
yoonyeobin <ugt9076@naver.com>
2024-11-14 16:58:39 +0900

    🩹 fix: 유저 찾는 부분 추가
    오타 수정 및 유저 찾는 부분 추가

--- src/handler/town/sChatHandler.js
+++ src/handler/town/sChatHandler.js
@@ -1,9 +1,16 @@
 import { PacketType } from "../../constants/header.js";
 import { createResponse } from "../../utils/response/createResponse.js";
+import { getUserBySocket } from "../../sessions/userSession.js";
 import { townSession } from "../../sessions/sessions.js";
 
 export const sChatHandler = async ({ socket, payload }) => {
-    const { playerId, senderName, chatMsg } = [payload]
+    const { playerId, senderName, chatMsg } = payload;
+
+    const user = await getUserBySocket(socket);
+    if (!user) {
+        console.error('유저를 찾을 수 없습니다.');
+        return;
+    }
 
     const data = {
         playerId: playerId,
@@ -13,6 +20,7 @@ export const sChatHandler = async ({ socket, payload }) => {
     const chatPayload = createResponse(PacketType.S_Chat, data)
     if (!townSession) {
         console.error('타운세션을 찾을 수 없습니다.')
+        return;
     }
     // 타운 내 자신을 제외한 모든 유저에게 패킷 전송
     townSession.users.forEach((targetUser) => {
@@ -20,7 +28,7 @@ export const sChatHandler = async ({ socket, payload }) => {
             try {
                 targetUser.socket.write(chatPayload);
             } catch (error) {
-                console.error('Smove 패킷 전송중 오류 발생', error)
+                console.error('S_Chat 패킷 전송중 오류 발생', error)
             }
         }
     });
