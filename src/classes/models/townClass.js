d3e37ee8fe756917eeabd1f43f0ffde405e37a73
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2024-11-14 21:53:56 +0900

    ✨ update 클래스 정의 변경

--- src/classes/models/townClass.js
+++ src/classes/models/townClass.js
@@ -1,16 +1,19 @@
 ﻿// src/classes/models/townClass.js
 
-import Game from './gameClass.js';
+import BaseSession from './BaseSessionClass.js';
 
 // Game 클래스 상속 받음
-class Town extends Game{
+class Town extends BaseSession {
   constructor(townSessionId) {
     super(townSessionId);
-    
+
     // 마을에 필요한 데이터들 추가
-    
+
     // 추후 NPC 추가
     // this.npcs = [];
+  }
+  addUser(user) {
+    this.users.push(user);
   }
 }
 
---
+++ src/sessions/dungeonSession.js
@@ -0,0 +1,29 @@
+// src/sessions/game.session.js
+
+import Dungeon from '../classes/models/dungeonClass.js';
+import { dungeonSessions } from './sessions.js';
+
+export const addDungeonSession = (sessionId, dungeonCode) => {
+  const session = new Dungeon(sessionId, dungeonCode);
+  dungeonSessions.push(session);
+  return session;
+};
+
+export const removeDungeonSession = (sessionId) => {
+  const index = dungeonSessions.findIndex((session) => session.sessionId === sessionId);
+  if (index !== -1) {
+    dungeonSessions.splice(index, 1);
+  }
+};
+
+export const getDungeonSession = () => {
+  return dungeonSessions;
+};
+
+export const getDungeonSessionById = (sessionId) => {
+  const dungeonSession = dungeonSessions.find((session) => session.sessionId === sessionId);
+  if (!game) {
+    console.error('dungeonSession not found');
+  }
+  return dungeonSession;
+};
