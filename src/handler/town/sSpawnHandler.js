565fb5b1e2c71a2e4414e8720b283949f186d91c
Dam2204 <soyoung4576@gmail.com>
2024-11-16 00:17:41 +0900

    ♻️ refactor: 플레이어 패킷 데이터 utils에 분리

--- src/handler/town/sSpawnHandler.js
+++ src/handler/town/sSpawnHandler.js
@@ -1,33 +1,13 @@
 import { PacketType } from '../../constants/header.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 import { getAllUserExceptMyself } from '../../sessions/townSession.js';
+import { playerData } from '../../utils/packet/playerPacket.js';
 
 export const sSpawnHandler = async (newUser) => {
   const otherUsers = await getAllUserExceptMyself(newUser.id);
 
   // 새로운 사용자 정보 생성
-  const newPlayerData = {
-    playerId: newUser.id,
-    nickname: newUser.nickname,
-    class: newUser.job,
-    transform: {
-      posX: newUser.position.posX,
-      posY: newUser.position.posY,
-      posZ: newUser.position.posZ,
-      rot: newUser.position.rot,
-    },
-    statInfo: {
-      level: newUser.level,
-      hp: newUser.stat.hp,
-      maxHp: newUser.stat.maxHp,
-      mp: newUser.stat.mp,
-      maxMp: newUser.stat.maxMp,
-      atk: newUser.stat.atk,
-      def: newUser.stat.def,
-      magic: newUser.stat.magic,
-      speed: newUser.stat.speed,
-    },
-  };
+  const newPlayerData = playerData(newUser);
 
   // S_Spawn 응답 생성
   const spawnResponse = createResponse(PacketType.S_Spawn, { players: [newPlayerData] });
