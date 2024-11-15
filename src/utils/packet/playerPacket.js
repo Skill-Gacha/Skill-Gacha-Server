0c6c6de10a958894d6950a0b930c1856ccb88091
Dam2204 <soyoung4576@gmail.com>
2024-11-15 22:42:24 +0900

    ♻️ refactor: 플레이어 패킷 데이터 utils에 분리

---
+++ src/utils/packet/playerPacket.js
@@ -0,0 +1,26 @@
+export const playerData = (user) => {
+  const playerInfo = {
+    playerId: user.id,
+    nickname: user.nickname,
+    class: user.job,
+    transform: {
+      posX: user.position.posX,
+      posY: user.position.posY,
+      posZ: user.position.posZ,
+      rot: user.position.rot,
+    },
+    statInfo: {
+      level: user.level,
+      hp: user.stat.hp,
+      maxHp: user.stat.maxHp,
+      mp: user.stat.mp,
+      maxMp: user.stat.maxMp,
+      atk: user.stat.atk,
+      def: user.stat.def,
+      magic: user.stat.magic,
+      speed: user.stat.speed,
+    },
+  };
+
+  return playerInfo;
+};
