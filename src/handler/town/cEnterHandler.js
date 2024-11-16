5fca0b7e00dbceed63ed6c7558d3d37715665119
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-16 19:28:56 +0900

    🩹 fix : 코드 수정

--- src/handler/town/cEnterHandler.js
+++ src/handler/town/cEnterHandler.js
@@ -29,7 +29,7 @@ export const cEnterHandler = async ({ socket, payload }) => {
     newUser = existingPlayer;
   } else {
     // 새로운 사용자 생성 및 DB에 저장
-    newUser = await createUser(
+    await createUser(
       nickname,
       jobClass,
       1, // 초기 레벨
@@ -40,6 +40,7 @@ export const cEnterHandler = async ({ socket, payload }) => {
       chosenJob.magic,
       chosenJob.speed,
     );
+    newUser = await findUserNickname(nickname);
   }
   // User 클래스 인스턴스 생성
   const user = new User(socket, newUser.id, nickname);
