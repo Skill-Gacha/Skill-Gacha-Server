81a666ec12dd641357284e9270e250cbbf7e1beb
Ayumudayo <yumemiayumu@gmail.com>
2024-11-20 15:26:27 +0900

    ✨ update: 서버 구동시 초기화 작업 추가

--- src/init/index.js
+++ src/init/index.js
@@ -3,12 +3,16 @@
 import testConnection from '../utils/db/testConnection.js';
 import { loadProtos } from './loadProto.js';
 import { loadGameAssets } from './loadAssets.js';
+import { initRedisClient } from './redis.js';
+import { startSyncScheduler } from '../schedulers/syncScheduler.js';
 
 const initServer = async () => {
   try {
     await loadProtos();
     await loadGameAssets();
     await testConnection();
+    await initRedisClient();
+    await startSyncScheduler();
   } catch (e) {
     console.error(e);
     process.exit(1);
