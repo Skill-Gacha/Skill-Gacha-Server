0748d5cc13a7e56930b2cdfea972b0ed53b6b61c
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-21 23:39:38 +0900

    🎉 add : 속성 추가 및 오타, 주석처리 (나중에 풀것)

--- src/classes/models/statClass.js
+++ src/classes/models/statClass.js
@@ -6,14 +6,14 @@ class Stat {
     this.maxHp = maxHp;
     this.mp = mp;
     this.maxMp = maxMp;
-    
-    this.resistances = {
-      electricResist: resists.electricResist,
-      earthResist: resists.earthResist,
-      grassResist: resists.grassResist,
-      fireResist: resists.fireResist,
-      waterResist: resists.waterResist,
-    };
+
+    // this.resistances = {
+    //   electricResist: resists.electricResist,
+    //   earthResist: resists.earthResist,
+    //   grassResist: resists.grassResist,
+    //   fireResist: resists.fireResist,
+    //   waterResist: resists.waterResist,
+    // };
 
 
     // this.resistances = {
