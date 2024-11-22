2b19dcbda708053066e2a868b4a78adee2202762
yoonyeobin <ugt9076@naver.com>
2024-11-22 12:50:54 +0900

    📚 docs: 기존 주석 제거

--- src/classes/models/statClass.js
+++ src/classes/models/statClass.js
@@ -14,14 +14,6 @@ class Stat {
       fireResist: resists.fireResist,
       waterResist: resists.waterResist,
     };
-
-    // this.resistances = {
-    //   water: resistances.water || 0, // 물 저항
-    //   fire: resistances.fire || 0, // 불 저항
-    //   grass: resistances.grass || 0, // 풀 저항
-    //   ground: resistances.ground || 0, // 땅 저항
-    //   electric: resistances.electric || 0, // 전기 저항
-    // };
   }
 }
 
