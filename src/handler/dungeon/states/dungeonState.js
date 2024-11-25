742fa809d8185da0a80c1220469943abdea9ecfe
Ayumudayo <yumemiayumu@gmail.com>
2024-11-25 14:25:18 +0900

    ♻️ refactor: PvP/PvE 공통 기능 통합

--- src/handler/dungeon/states/dungeonState.js
+++ src/handler/dungeon/states/dungeonState.js
@@ -1,33 +1,11 @@
 ﻿// src/handler/dungeon/states/dungeonState.js
 
-// DungeonState 클래스는 단독으로 사용 불가능
-// 모든 상태는 이 클래스를 상속받아 구현되어야 함
-// PvP도 PVPState 같은 식으로 만들어 할 수 있을듯?
-export default class DungeonState {
-  constructor(dungeon, user, socket) {
-    if (new.target === DungeonState) {
-      throw new Error('DungeonState는 추상 클래스입니다. 인스턴스를 생성할 수 없습니다.');
-    }
+import GameState from '../../states/gameState.js';
 
-    this.dungeon = dungeon;
-    this.user = user;
-    this.socket = socket;
+export default class DungeonState extends GameState {
+  constructor(session, user, socket) {
+    super(session, user, socket);
+    this.dungeon = session; // 세션을 던전으로 명시적으로 설정
   }
 
-  // 상태 전환 시 첫 호출되는 함수
-  async enter() {
-    throw new Error('enter()는 상속을 받아 구현해야 합니다.');
-  }
-
-  // 클라이언트로부터의 응답 코드를 처리하는 함수
-  async handleInput(responseCode) {
-    throw new Error('handleInput()는 상속을 받아 구현해야 합니다.');
-  }
-
-  // 상태를 바꿀 때 호출되는 함수
-  // 넘어가면 바로 enter()를 호출함
-  changeState(StateClass) {
-    this.dungeon.currentState = new StateClass(this.dungeon, this.user, this.socket);
-    this.dungeon.currentState.enter();
-  }
 }
--- src/handler/pvp/states/pvpState.js
+++ src/handler/pvp/states/pvpState.js
@@ -1,32 +1,17 @@
 ﻿// src/handler/pvp/states/pvpState.js
 
-// PvpState 클래스는 단독으로 사용 불가능
-// 모든 상태는 이 클래스를 상속받아 구현되어야 함
-// PvP도 PVPState 같은 식으로 만들어 할 수 있을듯?
-export default class PvpState {
-  constructor(pvpRoom, mover, stopper) {
-    if (new.target === PvpState) {
-      throw new Error('PvpState는 추상 클래스입니다. 인스턴스를 생성할 수 없습니다.');
-    }
+import GameState from '../../states/gameState.js';
 
-    this.pvpRoom = pvpRoom;
-    this.mover = mover; //행동을 할 사람
-    this.stopper = stopper; //행동을 할 기회가 아닌 사람
-  }
-  // 상태 전환 시 첫 호출되는 함수
-  async enter() {
-    throw new Error('enter()는 상속을 받아 구현해야 합니다.');
+export default class PvpState extends GameState {
+  constructor(session, mover, stopper) {
+    super(session, mover, mover.socket);
+    this.pvpRoom = session; // 세션을 PVP 룸으로 명시적으로 설정
+    this.mover = mover;
+    this.stopper = stopper;
   }
 
-  // 클라이언트로부터의 응답 코드를 처리하는 함수
-  async handleInput(responseCode) {
-    throw new Error('handleInput()는 상속을 받아 구현해야 합니다.');
-  }
-
-  // 상태를 바꿀 때 호출되는 함수
-  // 넘어가면 바로 enter()를 호출함
   changeState(StateClass) {
-    // mover랑 stopper 비교를 위해 실행 userTurn이 0이면
+    // 턴 교체 로직 추가
     const [playerA, playerB] = Array.from(this.pvpRoom.users.values());
 
     if (this.pvpRoom.getUserTurn() === 0) {
