0a0c99842a4a108b91cc99196a721c632a880629
Ayumudayo <yumemiayumu@gmail.com>
2024-11-25 15:15:21 +0900

    ♻️ refactor: 스케줄러 기능 코드 정리

--- src/sync/syncRatings.js
+++ src/sync/syncRatings.js
@@ -1,6 +1,5 @@
 ﻿// src/sync/syncRatings.js
 
-// 모든 플레이어의 레이팅 정보를 MySQL에 동기화
 import { saveRatingToDB } from '../db/rating/ratingDb.js';
 import { getAllRatingsFromRedis } from '../db/redis/ratingService.js';
 
@@ -8,7 +7,6 @@ export const syncRatingsToDB = async () => {
   try {
     console.log('레이팅 동기화 작업 시작...');
 
-    // Redis에서 모든 레이팅 정보 가져오기
     const allRatings = await getAllRatingsFromRedis();
 
     if (allRatings.length === 0) {
@@ -16,13 +14,14 @@ export const syncRatingsToDB = async () => {
       return;
     }
 
-    // MySQL에 레이팅 정보 저장
-    const savePromises = allRatings.map(({ value: nickname, score: rating }) => saveRatingToDB(nickname, rating));
+    const savePromises = allRatings.map(({ value: nickname, score: rating }) =>
+      saveRatingToDB(nickname, rating)
+    );
     await Promise.all(savePromises);
 
     console.log('레이팅 동기화 작업 완료.');
   } catch (error) {
-    console.error('레이팅 동기화 중 오류 발생:', error);
-    throw error; // 필요 시 외부로 에러를 던짐
+    console.error('syncRatings: 레이팅 동기화 중 오류 발생:', error);
+    throw error;
   }
 };
