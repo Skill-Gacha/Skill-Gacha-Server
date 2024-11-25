262e8861d52581bb601ce83366065fcc24f2002b
Ayumudayo <yumemiayumu@gmail.com>
2024-11-25 14:56:46 +0900

    🩹 fix: 쿼리문 케이스 맞춤

--- src/db/user/user.db.js
+++ src/db/user/user.db.js
@@ -1,7 +1,7 @@
 // src/db/user/user.db.js
 
-import { toCamelCase } from '../../utils/transformCase.js';
 import dbPool from '../database.js';
+import { toCamelCase } from '../../utils/transformCase.js';
 import { USER_QUERIES } from './user.queries.js';
 
 export const findUserNickname = async (nickname) => {
@@ -10,9 +10,9 @@ export const findUserNickname = async (nickname) => {
   return toCamelCase(rows[0]);
 };
 
-export const createUser = async (nickname, element, maxhp, maxmp, gold = 0, stone = 0) => {
-  await dbPool.query(USER_QUERIES.CREATE_USER, [nickname, element, maxhp, maxmp, gold, stone]);
-  return { nickname, element, maxhp, maxmp, gold, stone };
+export const createUser = async (nickname, element, maxHp, maxMp, gold = 0, stone = 0) => {
+  await dbPool.query(USER_QUERIES.CREATE_USER, [nickname, element, maxHp, maxMp, gold, stone]);
+  return { nickname, element, maxHp, maxMp, gold, stone };
 };
 
 export const updateUserResource = async (nickname, gold, stone) => {
