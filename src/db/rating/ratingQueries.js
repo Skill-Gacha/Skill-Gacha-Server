2246426c5a5e95c71f53b76ebe948805640ab13b
Ayumudayo <yumemiayumu@gmail.com>
2024-11-25 14:57:12 +0900

    🩹 fix: 쿼리문 인덴트 정리

--- src/db/rating/ratingQueries.js
+++ src/db/rating/ratingQueries.js
@@ -1,13 +1,12 @@
-﻿// src/db/rating/ratingService.js
+﻿// src/db/rating/ratingQueries.js
 
 export const RATING_QUERIES = {
   GET_RATING_BY_NICKNAME: 'SELECT rating FROM Ratings WHERE nickname = ?',
   SAVE_RATING: `
       INSERT INTO Ratings (nickname, rating, updatedat)
-      VALUES (?, ?, CURRENT_TIMESTAMP) ON DUPLICATE KEY
-      UPDATE
-          rating =
-      VALUES (rating), updatedat =
-      VALUES (updatedat)
+      VALUES (?, ?, CURRENT_TIMESTAMP)
+          ON DUPLICATE KEY UPDATE
+            rating = VALUES(rating),
+            updatedat = VALUES(updatedat)
   `,
 };
