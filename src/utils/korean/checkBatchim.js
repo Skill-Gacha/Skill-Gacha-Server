91edc8c51a916b3146b5d19db261336da5776a0b
Ayumudayo <yumemiayumu@gmail.com>
2024-11-26 15:41:28 +0900

    📚 docs: 주석 추가

--- src/utils/korean/checkBatchim.js
+++ src/utils/korean/checkBatchim.js
@@ -4,11 +4,14 @@
 function checkBatchim(word) {
   if (typeof word !== 'string') return null;
   
+  // 마지막 글자 가져옴
   const lastChar = word[word.length - 1];
   const code = lastChar.charCodeAt(0);
   
+  // 한글인지 검사
   if (code < 44032 || code > 55203) return null; // 한글 범위 외의 문자
   
+  // 결과 리턴
   return (code - 44032) % 28 !== 0;
 }
 
