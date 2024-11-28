c47be1ec6040d2b0abfb315f63a70a7197838f0a
Ayumudayo <yumemiayumu@gmail.com>
2024-11-28 20:18:33 +0900

    ♻️ refactor: 상수화 및 스테이트 매핑을 통한 개선

--- src/handler/pvp/states/pvpActionState.js
+++ src/handler/pvp/states/pvpActionState.js
@@ -6,53 +6,48 @@ import PvpState from './pvpState.js';
 import { invalidResponseCode } from '../../../utils/error/invalidResponseCode.js';
 import PvpSkillChoice from './pvpSkillChoiceState.js';
 import PvpItemChoiceState from './pvpItemChoiceState.js';
+import PvpIncreaseManaState from './pvpIncreaseManaState.js';
 import { PacketType } from '../../../constants/header.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
-import PvpIncreaseManaState from './pvpIncreaseManaState.js';
+
+const BUTTON_OPTIONS = ['스킬 사용', '아이템 사용', '턴 넘기기', '도망치기'];
 
 export default class PvpActionState extends PvpState {
   enter() {
     this.pvpRoom.pvpStatus = PVP_STATUS.ACTION;
     if (this.pvpRoom.gameStart) {
-      const buttons = [
-        { msg: '스킬 사용', enable: true },
-        { msg: '아이템 사용', enable: true },
-        { msg: '턴 넘기기', enable: true },
-        { msg: '도망치기', enable: true },
-      ];
-
       const battleLog = {
         msg: '행동을 선택해주세요.',
         typingAnimation: false,
-        btns: buttons,
+        btns: BUTTON_OPTIONS.map((msg) => ({ msg, enable: true })),
       };
 
-      const actionChooseBattlelogResponse = createResponse(PacketType.S_PvpBattleLog, {
-        battleLog,
-      });
-      this.mover.socket.write(actionChooseBattlelogResponse);
+      const response = createResponse(PacketType.S_PvpBattleLog, { battleLog });
+      this.mover.socket.write(response);
     }
     this.pvpRoom.gameStart = true;
   }
 
+  // switch-case 대신 매핑으로 가독성 개선
   async handleInput(responseCode) {
-    switch (responseCode) {
-      case 1: //스킬
-        this.changeState(PvpSkillChoice);
-        break;
-      case 2: // 아이템
-        this.changeState(PvpItemChoiceState);
-        break;
-      case 3: // 턴 넘기기
-        this.changeState(PvpIncreaseManaState);
-        break;
-      case 4: // 도망치기
-        this.changeState(PvpConfirmState);
-        await this.pvpRoom.currentState.setConfirm(CONFIRM_TYPE.FLEE, '추하게 빼실겁니까?');
-        break;
-      default:
-        invalidResponseCode(this.mover.socket);
-        break;
+    const actionMap = {
+      1: PvpSkillChoice,
+      2: PvpItemChoiceState,
+      3: PvpIncreaseManaState,
+      4: PvpConfirmState,
+    };
+
+    const SelectedState = actionMap[responseCode];
+    if (!SelectedState) {
+      invalidResponseCode(this.mover.socket);
+      return;
+    }
+
+    if (SelectedState === PvpConfirmState) {
+      this.changeState(SelectedState);
+      await this.pvpRoom.currentState.setConfirm(CONFIRM_TYPE.FLEE, '추하게 빼실겁니까?');
+    } else {
+      this.changeState(SelectedState);
     }
   }
 }
