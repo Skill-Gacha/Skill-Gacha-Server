9876be5891310c6133094b8f701d3686ee35eab4
Ayumudayo <yumemiayumu@gmail.com>
2024-11-28 22:40:47 +0900

    ♻️ refactor: 상수화로 가독성 개선

--- src/handler/pvp/states/pvpTurnChangeState.js
+++ src/handler/pvp/states/pvpTurnChangeState.js
@@ -6,6 +6,23 @@ import { createResponse } from '../../../utils/response/createResponse.js';
 import { PacketType } from '../../../constants/header.js';
 import { PVP_STATUS } from '../../../constants/battle.js';
 
+const TURN_CHANGE_BUTTONS_MOVER = [
+  { msg: '스킬 사용', enable: false },
+  { msg: '아이템 사용', enable: false },
+  { msg: '도망치기', enable: false },
+];
+
+const TURN_CHANGE_BUTTONS_STOPPER = [
+  { msg: '스킬 사용', enable: true },
+  { msg: '아이템 사용', enable: true },
+  { msg: '도망치기', enable: true },
+];
+
+const TURN_CHANGE_MESSAGES = {
+  mover: '이제 님이 맞을 차례예요',
+  stopper: '이제 님이 때릴 차례예요',
+};
+
 export default class PvpTurnChangeState extends PvpState {
   enter() {
     this.pvpRoom.pvpStatus = PVP_STATUS.TURN_CHANGE;
@@ -24,23 +41,15 @@ export default class PvpTurnChangeState extends PvpState {
 
     // 배틀 로그 전송
     const moverBattleLog = {
-      msg: '이제 님이 맞을 차례예요',
+      msg: TURN_CHANGE_MESSAGES.mover,
       typingAnimation: false,
-      btns: [
-        { msg: '스킬 사용', enable: false },
-        { msg: '아이템 사용', enable: false },
-        { msg: '도망치기', enable: false },
-      ],
+      btns: TURN_CHANGE_BUTTONS_MOVER,
     };
 
     const stopperBattleLog = {
-      msg: '이제 님이 때릴 차례예요',
+      msg: TURN_CHANGE_MESSAGES.stopper,
       typingAnimation: false,
-      btns: [
-        { msg: '스킬 사용', enable: true },
-        { msg: '아이템 사용', enable: true },
-        { msg: '도망치기', enable: true },
-      ],
+      btns: TURN_CHANGE_BUTTONS_STOPPER,
     };
 
     this.mover.socket.write(
@@ -53,7 +62,8 @@ export default class PvpTurnChangeState extends PvpState {
 
     // 다음 상태로 전환하면서 mover와 stopper 교체
     this.changeState(PvpActionState, true);
-    
+
+    // 타이머 관리
     this.pvpRoom.clearTurnTimer();
     this.pvpRoom.startTurnTimer();
   }
