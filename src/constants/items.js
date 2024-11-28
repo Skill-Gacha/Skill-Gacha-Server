e6a1b252ae8772e339000ac0d457c5c49263a538
Ayumudayo <yumemiayumu@gmail.com>
2024-11-28 20:51:21 +0900

    🎉 add: 아이템 정의 상수 추가

---
+++ src/constants/items.js
@@ -0,0 +1,10 @@
+﻿// src/constants/items.js
+
+export const ITEM_TYPES = {
+  HP_POTION: 4001,
+  MP_POTION: 4002,
+  BERSERK_POTION: 4003,
+  DANGER_POTION: 4004,
+  PANACEA: 4005,
+  // 필요한 아이템 추가
+};
