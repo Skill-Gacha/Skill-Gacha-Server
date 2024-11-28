f35cd2e5591075a0e7991ff2f9fb57637f4ea32d
Ayumudayo <yumemiayumu@gmail.com>
2024-11-28 21:05:03 +0900

    🩹 fix: `pvpFleeMessageState`와 상수 이름 통일

--- src/handler/pvp/states/pvpGameOverState.js
+++ src/handler/pvp/states/pvpGameOverState.js
@@ -8,7 +8,7 @@ import PvpState from './pvpState.js';
 import { getPlayerRatingFromRedis, updatePlayerRating } from '../../../db/redis/ratingService.js';
 import { invalidResponseCode } from '../../../utils/error/invalidResponseCode.js';
 
-const POINT_CHANGE = 10;
+const RANK_CHANGE_POINTS = 10;
 
 export default class PvpGameOverState extends PvpState {
   async enter() {
@@ -21,8 +21,8 @@ export default class PvpGameOverState extends PvpState {
       ]);
 
       await Promise.all([
-        updatePlayerRating(this.mover.nickname, winnerRating + POINT_CHANGE),
-        updatePlayerRating(this.stopper.nickname, loserRating - POINT_CHANGE),
+        updatePlayerRating(this.mover.nickname, winnerRating + RANK_CHANGE_POINTS),
+        updatePlayerRating(this.stopper.nickname, loserRating - RANK_CHANGE_POINTS),
       ]);
     } catch (error) {
       console.error('pvpGameOverState: 랭크 점수 업데이트 중 오류 발생:', error);
