1f14ca55e5815947b18fd047e0eef43dbae84556
Ayumudayo <yumemiayumu@gmail.com>
2024-11-28 11:26:19 +0900

    ♻️ refactor: 하나의 함수에서 다 처리하도록 수정

--- src/db/redis/ratingService.js
+++ src/db/redis/ratingService.js
@@ -24,8 +24,49 @@ export const getPlayerRatingFromRedis = async (nickname) => {
 };
 
 export const getTopRatings = async (topN) => {
-  return await redisClient.zRangeWithScores(RATING_KEY, -topN, -1, { REV: true });
+  return await redisClient.zRangeWithScores(RATING_KEY, 0, topN - 1, { REV: true });
 };
+
+export const getTopRatingsWithPlayer = async (nickname, topN) => {
+  try {
+    // 상위 topN 데이터 가져오기 (점수 내림차순)
+    const topRatings = await redisClient.zRangeWithScores(RATING_KEY, 0, topN - 1, { REV: true });
+
+    // 각 레이팅에 랭크 정보 추가
+    topRatings.forEach((entry, index) => {
+      entry.rank = index + 1; // 랭크는 1부터 시작
+    });
+
+    // 타겟이 상위 리스트에 포함되어 있는지 확인
+    const isNicknameInTop = topRatings.some((entry) => entry.value === nickname);
+
+    if (!isNicknameInTop) {
+      // 타겟의 정보 조회
+      const [playerRank, playerScore] = await Promise.all([
+        redisClient.zRevRank(RATING_KEY, nickname),
+        redisClient.zScore(RATING_KEY, nickname),
+      ]);
+
+      if (playerRank !== null && playerScore !== null) {
+        // 타겟의 레이팅 정보를 리스트에 추가
+        topRatings.push({
+          value: nickname,
+          score: playerScore,
+          rank: playerRank + 1, // Redis의 ZRevRank는 0부터 시작하므로 1을 더함
+        });
+
+        // 추가되면 topN+1개
+        return topRatings.slice(0, topN + 1);
+      }
+    }
+
+    return topRatings;
+  } catch (error) {
+    console.error('ratingService: getTopRatingsWithPlayer: 레이팅 정보를 가져오는 중 오류 발생', error);
+    throw error;
+  }
+};
+
 
 export const getPlayerRank = async (nickname) => {
   const rank = await redisClient.zRevRank(RATING_KEY, nickname);
