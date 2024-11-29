6fe6269c2726976a8792e0a654b92ae8c3553638
Ayumudayo <yumemiayumu@gmail.com>
2024-11-29 23:56:26 +0900

    ♻️ refactor: 브로드캐스트 함수 추가 및 코드 정리
    - 채팅 메세지 검증 추가
    - 에러 발생 시 `throw`하게 수정

--- src/handler/town/cChatHandler.js
+++ src/handler/town/cChatHandler.js
@@ -5,31 +5,42 @@ import { createResponse } from '../../utils/response/createResponse.js';
 import sessionManager from '#managers/sessionManager.js';
 
 export const cChatHandler = async ({ socket, payload }) => {
-  const { chatMsg } = payload;
+  try {
+    const { chatMsg } = payload;
 
-  const user = sessionManager.getUserBySocket(socket);
-  if (!user) {
-    console.error('cChatHandler: 유저를 찾을 수 없습니다.');
-    return;
+    if (typeof chatMsg !== 'string' || chatMsg.trim() === '') {
+      throw new Error('cChatHandler: 유효하지 않은 채팅');
+    }
+
+    const user = sessionManager.getUserBySocket(socket);
+    if (!user) {
+      throw new Error('cChatHandler: 유저를 찾을 수 없습니다.');
+    }
+
+    const town = sessionManager.getTown();
+    if (!town) {
+      throw new Error('cChatHandler: 타운 세션을 찾을 수 없습니다.');
+    }
+
+    const chatPayload = createResponse(PacketType.S_Chat, {
+      playerId: user.id,
+      chatMsg: chatMsg,
+    });
+
+    // 타운 내 모든 유저에게 패킷 전송
+    broadcastToTown(town, chatPayload);
+  } catch (error) {
+    console.error(`cChatHandler 에러 발생: ${error.message}`);
+    // 필요한 경우 사용자에게 에러 응답 전송
   }
+};
 
-  const chatPayload = createResponse(PacketType.S_Chat, {
-    playerId: user.id,
-    chatMsg: chatMsg,
-  });
-
-  const town = sessionManager.getTown();
-  if (!town) {
-    console.error('cChatHandler: 타운 세션을 찾을 수 없습니다.');
-    return;
-  }
-
-  // 타운 내 모든 유저에게 패킷 전송
+const broadcastToTown = (town, payload) => {
   town.users.forEach((targetUser) => {
     try {
-      targetUser.socket.write(chatPayload);
+      targetUser.socket.write(payload);
     } catch (error) {
-      console.error('cChatHandler: S_Chat 패킷 전송 중 오류 발생:', error);
+      console.error(`cChatHandler: S_Chat 패킷 전송 중 오류 발생: ${targetUser.id}: ${error.message}`);
     }
   });
 };
