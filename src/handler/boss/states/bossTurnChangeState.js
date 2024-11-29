5ae0f799473a187a8430cb56a301e771e97a0f1b
yoonyeobin <ugt9076@naver.com>
2024-11-29 18:40:27 +0900

    ✨ update: 보스룸 턴 교체 로직 구현

--- src/handler/boss/states/bossTurnChangeState.js
+++ src/handler/boss/states/bossTurnChangeState.js
@@ -6,6 +6,20 @@ import BossRoomState from './bossRoomState.js';
 export default class BossTurnChangeState extends BossRoomState {
   async enter() {
     this.bossRoom.bossStatus = BOSS_STATUS.TURN_CHANGE;
+
+    // 현재 유저의 인덱스
+    let currentIdx = this.users.findIndex((user) => user.id === this.user.id);
+
+    if (currentIdx === -1) {
+      console.error('존재하지 않는 유저입니다.');
+      return;
+    }
+
+    // 최소 한번은 실행하고 살아있는 유저를 찾아 턴 넘겨주기
+    do {
+      const currentIdx = (currentIdx + 1) % this.users.length;
+      this.user = this.users[currentIdx];
+    } while (this.user.isDead);
   }
 
   async handleInput(responseCode) {}
