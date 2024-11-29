49d43420f93a9ee8d201b8ac3d694071ff8c0552
Ayumudayo <yumemiayumu@gmail.com>
2024-11-29 15:03:04 +0900

    ♻️ refactor: 초기화 부분 함수화 및 일부 코드 정리

--- src/handler/dungeon/cPlayerResponseHandler.js
+++ src/handler/dungeon/cPlayerResponseHandler.js
@@ -5,7 +5,6 @@ import { handleError } from '../../utils/error/errorHandler.js';
 
 export const cPlayerResponseHandler = async ({ socket, payload }) => {
   const user = sessionManager.getUserBySocket(socket);
-  const responseCode = payload.responseCode || 0;
 
   if (!user) {
     console.error('cPlayerResponseHandler: 유저를 찾을 수 없습니다.');
@@ -21,21 +20,29 @@ export const cPlayerResponseHandler = async ({ socket, payload }) => {
 
   if (!dungeon.currentState) {
     // 초기 상태 설정
-    try {
-      const MessageState = (await import('./states/messageState.js')).default;
-      dungeon.currentState = new MessageState(dungeon, user, socket);
-      await dungeon.currentState.enter();
-    } catch (error) {
-      console.error('cPlayerResponseHandler: 초기 상태 설정 중 오류 발생:', error);
-      handleError(error);
-      return;
-    }
+    const initializedDungeonState = await initializeDungeonState(dungeon, user, socket);
+    if (!initializedDungeonState) return;
   }
+
+  const responseCode = payload.responseCode !== undefined ? payload.responseCode : 0;
 
   try {
     await dungeon.currentState.handleInput(responseCode);
   } catch (error) {
     console.error('cPlayerResponseHandler: 처리 중 오류 발생:', error);
     handleError(error);
+  }
+};
+
+const initializeDungeonState = async (dungeon, user, socket) => {
+  try {
+    const MessageState = (await import('./states/messageState.js')).default;
+    dungeon.currentState = new MessageState(dungeon, user, socket);
+    await dungeon.currentState.enter();
+    return true;
+  } catch (error) {
+    console.error('cPlayerResponseHandler: 초기 상태 설정 중 오류 발생:', error);
+    handleError(error);
+    return false;
   }
 };
