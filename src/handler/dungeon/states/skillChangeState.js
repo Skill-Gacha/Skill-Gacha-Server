b0c80dbfbd1ec3e2b896d7597043ed3e3486a14b
Ayumudayo <yumemiayumu@gmail.com>
2024-11-29 22:20:09 +0900

    ♻️ refactor: 코드 정리 및 불필요 주석 일부 제거

--- src/handler/dungeon/states/skillChangeState.js
+++ src/handler/dungeon/states/skillChangeState.js
@@ -8,51 +8,56 @@ import { CONFIRM_TYPE, DUNGEON_STATUS, MAX_BUTTON_COUNT } from '../../../constan
 import { invalidResponseCode } from '../../../utils/error/invalidResponseCode.js';
 import RewardState from './rewardState.js';
 
-// 스킬 교체 처리
+const BUTTON_BACK = '뒤로 가기';
+
 export default class SkillChangeState extends DungeonState {
   async enter() {
-    // 버튼은 플레이어가 보유한 스킬들로 생성
     this.dungeon.dungeonStatus = DUNGEON_STATUS.SKILL_CHANGE;
+
     const buttons = this.user.userSkills.map((skill) => ({
       msg: skill.skillName,
       enable: true,
     }));
 
     buttons.push({
-      msg: '뒤로 가기',
+      msg: BUTTON_BACK,
       enable: true,
     });
 
-    // 교환로그 데이터
     const battleLog = {
       msg: '선택된 스킬은 삭제되며 새로운 스킬이 추가됩니다.',
       typingAnimation: false,
       btns: buttons,
     };
 
-    const changeSkillBattlelogResponse = createResponse(PacketType.S_BattleLog, { battleLog });
-    this.socket.write(changeSkillBattlelogResponse);
+    this.socket.write(
+      createResponse(PacketType.S_BattleLog, { battleLog }),
+    );
   }
 
   async handleInput(responseCode) {
-    // responseCode 유효성 검사
-    if (responseCode < 1 || responseCode > MAX_BUTTON_COUNT) {
+    if (!this.isValidResponseCode(responseCode)) {
       invalidResponseCode(this.socket);
+      return;
     }
 
-    if (responseCode === 5) {
+    if (responseCode === this.user.userSkills.length + 1) { // 뒤로 가기 버튼
       this.changeState(RewardState);
-    } else {
-      const deleteSkillIdx = responseCode - 1;
-      const newSkill = this.dungeon.newSkill;
-
-      // 한번 더 확인하기
-      this.changeState(ConfirmState);
-      await this.dungeon.currentState.setConfirm(
-        CONFIRM_TYPE.SKILLCHANGE,
-        `${this.user.userSkills[deleteSkillIdx].skillName} 스킬과 교환하시겠습니까?`,
-      );
-      await this.dungeon.currentState.setChangeSkill(deleteSkillIdx, newSkill);
+      return;
     }
+
+    const deleteSkillIdx = responseCode - 1;
+    const newSkill = this.dungeon.newSkill;
+
+    this.changeState(ConfirmState);
+    await this.dungeon.currentState.setConfirm(
+      CONFIRM_TYPE.SKILLCHANGE,
+      `${this.user.userSkills[deleteSkillIdx].skillName} 스킬과 교환하시겠습니까?`,
+    );
+    await this.dungeon.currentState.setChangeSkill(deleteSkillIdx, newSkill);
+  }
+
+  isValidResponseCode(code) {
+    return code >= 1 && code <= this.user.userSkills.length + 1;
   }
 }
