b75dbed74f8f72ec13eb81cdd6d23af2c6199d91
yoonyeobin <ugt9076@naver.com>
2024-11-29 18:41:23 +0900

    🩹 fix: 오타 수정

--- src/handler/boss/states/bossMonsterDeadState.js
+++ src/handler/boss/states/bossMonsterDeadState.js
@@ -2,16 +2,20 @@
 
 import { BOSS_STATUS } from '../../../constants/battle.js';
 import BossRoomState from './bossRoomState.js';
+import { PacketType } from '../../../constants/header.js';
+import { createResponse } from '../../../utils/response/createResponse.js';
+import BossGameOverWinState from './bossGameOverWinState.js';
+import BossEnemyAttackState from './bossEnemyAttackState.js';
 
 export default class BossMonsterDeadState extends BossRoomState {
   async enter() {
     this.bossRoom.bossStatus = BOSS_STATUS.MONSTER_DEAD;
-    const deadMonsters = this.dungeon.monsters.filter((m) => m.monsterHp <= 0 && !m.isDead);
+    const deadMonsters = this.bossRoom.monsters.filter((m) => m.monsterHp <= 0 && !m.isDead);
 
     // 모든 사망 몬스터에 대해 애니메이션 전송
     deadMonsters.forEach((monster) => {
       this.socket.write(
-        createResponse(PacketType.S_MonsterAction, {
+        createResponse(PacketType.S_BossMonsterAction, {
           actionMonsterIdx: monster.monsterIdx,
           actionSet: {
             animCode: 4, // 사망 애니메이션 코드
@@ -28,10 +32,10 @@ export default class BossMonsterDeadState extends BossRoomState {
 
     if (aliveMonsters.length === 0) {
       // 모든 몬스터가 사망한 경우 보상 상태로 전환
-      this.changeState(rewardState);
+      this.changeState(BossGameOverWinState);
     } else {
       // 아직 살아있는 몬스터가 있으면 적 공격 상태로 전환
-      this.changeState(EnemyAttackState);
+      this.changeState(BossEnemyAttackState);
     }
   }
 
