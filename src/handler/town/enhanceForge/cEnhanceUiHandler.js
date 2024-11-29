31de6e4cadc0267b6698f919f757274f7a913fd2
Ayumudayo <yumemiayumu@gmail.com>
2024-11-30 00:15:52 +0900

    ♻️ refactor: 코드 정리

--- src/handler/town/enhanceForge/cEnhanceUiHandler.js
+++ src/handler/town/enhanceForge/cEnhanceUiHandler.js
@@ -5,32 +5,24 @@ import { PacketType } from '../../../constants/header.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
 
 export const cEnhanceUiHandler = async ({ socket }) => {
-  const user = sessionManager.getUserBySocket(socket);
-
-  if (!user) {
-    console.error('cEnhanceUiHandler: 사용자가 속한 세션을 찾을 수 없습니다.');
-    return;
-  }
-
   try {
-    // 사용자의 스킬 정보 가져오기
-    const userSkills = user.userSkills;
+    const user = sessionManager.getUserBySocket(socket);
+    if (!user) {
+      throw new Error('cEnhanceUiHandler: 사용자를 찾을 수 없습니다.');
+    }
 
     // 사용자의 스킬 코드 목록 생성
-    const skillCodes = userSkills.map(skill => skill.id); // 사용자 스킬에서 ID 추출
+    const skillCodes = user.userSkills.map((skill) => skill.id);
 
     const enhanceUiData = {
-      gold: user.gold, // 보유 골드
-      stone: user.stone, // 보유 강화석
-      skillCode: skillCodes, // 보유한 스킬 코드 목록
+      gold: user.gold,
+      stone: user.stone,
+      skillCode: skillCodes,
     };
 
-    // 응답 패킷 생성
-    const enhanceUiResponse = createResponse(PacketType.S_EnhanceUiResponse, enhanceUiData);
-
-    // 사용자에게 응답 전송
-    socket.write(enhanceUiResponse);
+    // 응답 패킷 생성 및 전송
+    socket.write(createResponse(PacketType.S_EnhanceUiResponse, enhanceUiData));
   } catch (error) {
-    console.error('cEnhanceUiHandler: 스킬 정보를 가져오는 중 오류 발생:', error);
+    console.error(`cEnhanceUiHandler 에러 발생: ${error.message}`);
   }
 };
