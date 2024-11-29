fca265e7c248af70c27bc2741c643641d9c1c266
UIHyeonLEE <zlzmfdkdl@naver.com>
2024-11-29 20:40:39 +0900

    🎉 add : 초기 세팅(수정필요)

--- src/handler/boss/states/bossTargetState.js
+++ src/handler/boss/states/bossTargetState.js
@@ -5,13 +5,21 @@ import BossRoomState from './bossRoomState.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
 import { PacketType } from '../../../constants/header.js';
 
+
+// 공격할 대상을 선택하기 위한 버튼 목록 생성
 export default class BossTargetState extends BossRoomState {
   async enter() {
-    this.bossRoom.bossStatus = BOSS_STATUS.TARGET;
 
-    // 공격할 대상을 선택하기 위한 버튼 목록 생성
-    const buttons = this.createTargetButtons();
+     // 보스룸 인스턴스 생성 및 몬스터 추가
+    if (!this.bossRoom) {
+      this.bossRoom = new BossRoomClass(this.bossRoom.id); // 보스룸 생성
+      this.bossRoom.setBoss(); //보스
+      this.bossRoom.spawnMinions(); //쫄생성
+    }
     
+    this.bossRoom.bossStatus = BOSS_STATUS.TARGET;
+    const buttons = this.createTargetButtons();
+
     const battleLog = {
       msg: '공격할 대상을 선택해주세요.',
       typingAnimation: false,
@@ -22,52 +30,49 @@ export default class BossTargetState extends BossRoomState {
   }
 
   createTargetButtons() {
-    const monsters = this.bossRoom.monsters; // 보스룸의 몬스터 목록
-    const buttons = [];
-
-    // 몬스터 버튼 추가
-    monsters.forEach((monster) => {
-      buttons.push({
-        msg: monster.monsterName,
-        enable: monster.monsterHp > 0,
-      });
-    });
-
-    // 쫄 버튼 추가 (2페이즈에서만)
-    if (this.bossRoom.phase === 2) {
-      monsters.forEach((minion) => {
-        buttons.push({
-          msg: minion.monsterName,
-          enable: minion.monsterHp > 0,
-        });
-      });
-    }
-
-    return buttons;
+    return this.bossRoom.monsters.map((monster) => ({
+      msg: monster.monsterName,
+      enable: monster.monsterHp > 0,
+    }));
   }
 
   async handleInput(responseCode) {
-    const targetIndex = responseCode - 1; // 사용자가 선택한 인덱스
-    const allTargets = [...this.bossRoom.monsters, ...this.bossRoom.minions]; // 몬스터와 쫄을 합친 배열
+    const targetIndex = responseCode;
+    const allTargets = this.bossRoom.monsters; // 몬스터 배열
+
+    if (targetIndex < 0 || targetIndex >= allTargets.length) {
+      return this.invalidTargetResponse(targetIndex);
+    }
 
     const target = allTargets[targetIndex];
     if (target && target.monsterHp > 0) {
       this.bossRoom.selectedMonster = target;
       this.changeState(PlayerAttackState); // 공격 상태로 전환
     } else {
-      // 유효하지 않은 응답 코드 처리
-      // (예: 잘못된 인덱스, HP가 0인 경우)
-      this.invalidTargetResponse();
+      this.invalidTargetResponse(targetIndex);
     }
   }
 
-  invalidTargetResponse() {
-    const message = createResponse(PacketType.S_BossBattleLog, {
-      msg: '유효하지 않은 선택입니다. 다시 선택해주세요.',
+  invalidTargetResponse(targetIndex) {
+    let message = '유효하지 않은 선택입니다. 다시 선택해주세요.';
+    
+    // 인덱스 범위 초과 확인
+    const totalTargets = this.bossRoom.monsters.length;
+    if (targetIndex < 0 || targetIndex >= totalTargets) {
+      message = '선택한 인덱스가 유효하지 않습니다. 다시 선택해주세요.';
+    } else {
+      const target = this.bossRoom.monsters[targetIndex];
+      if (target.monsterHp === 0) {
+        message = `${target.monsterName}은(는) 이미 처치되었습니다. 다른 대상을 선택해주세요.`;
+      }
+    }
+
+    const response = createResponse(PacketType.S_BossBattleLog, {
+      msg: message,
       typingAnimation: false,
       btns: [],
     });
 
-    this.socket.write(message);
+    this.socket.write(response);
   }
 }
