065fb53dcd0481faf0c7cca83ff8ff349efed28f
JinYunSe <123576244+JinYunSe@users.noreply.github.com>
2024-12-02 00:11:46 +0900

    fix 오류 수정

--- src/handler/boss/states/bossRoomState.js
+++ src/handler/boss/states/bossRoomState.js
@@ -3,8 +3,8 @@
 import GameState from '../../states/gameState.js';
 
 export default class BossRoomState extends GameState {
-  constructor(session) {
-    super(session, user);
+  constructor(session, user, socket) {
+    super(session, user, socket);
     this.bossRoom = session;
     this.user = this.bossRoom.userTurn;
     this.users = this.bossRoom.getUsers();
