e314ab306d1b123631f35dc74dcfe30a9ba2265e
Ayumudayo <yumemiayumu@gmail.com>
2024-12-03 17:35:51 +0900

    🐛 bugfix: PvP 턴 구분에 간헐적 문제가 발생하던 부분 수정

--- src/handler/pvp/cPlayerMatchHandler.js
+++ src/handler/pvp/cPlayerMatchHandler.js
@@ -42,7 +42,8 @@ export const cPlayerMatchHandler = async ({ socket }) => {
 
   const dungeonCode = Math.floor(Math.random() * DUNGEON_CODE_RANGE + 1) + DUNGEON_CODE_BASE;
   pvpRoom.initializeTurn();
-  const isFirstAttack = pvpRoom.getUserTurn();
+  const isPlayerAFirstAttack = pvpRoom.getUserTurn() === 0;
+  const isPlayerBFirstAttack = pvpRoom.getUserTurn() === 1;
 
   const lastKoreanA = checkBatchim(playerB.nickname) ? '과' : '와';
   const lastKoreanB = checkBatchim(playerA.nickname) ? '과' : '와';
@@ -52,9 +53,14 @@ export const cPlayerMatchHandler = async ({ socket }) => {
     playerData: MyStatus(playerA),
     opponentData: OpponentStatus(playerB),
     battleLog: createBattleLogResponse(
-      generateBattleLog(playerB.nickname, lastKoreanA, isFirstAttack, '선공입니다.'),
-      isFirstAttack,
-      [true, true, true, true],
+      generateBattleLog(
+        playerB.nickname,
+        lastKoreanA,
+        isPlayerAFirstAttack,
+        isPlayerAFirstAttack ? '선공입니다.' : '후공입니다.'
+      ),
+      isPlayerAFirstAttack,
+      isPlayerAFirstAttack ? [true, true, true, true] : [false, false, false, false]
     ),
   });
 
@@ -63,9 +69,14 @@ export const cPlayerMatchHandler = async ({ socket }) => {
     playerData: MyStatus(playerB),
     opponentData: OpponentStatus(playerA),
     battleLog: createBattleLogResponse(
-      generateBattleLog(playerA.nickname, lastKoreanB, !isFirstAttack, '후공입니다.'),
-      !isFirstAttack,
-      [false, false, false, false],
+      generateBattleLog(
+        playerA.nickname,
+        lastKoreanB,
+        isPlayerBFirstAttack,
+        isPlayerBFirstAttack ? '선공입니다.' : '후공입니다.'
+      ),
+      isPlayerBFirstAttack,
+      isPlayerBFirstAttack ? [true, true, true, true] : [false, false, false, false]
     ),
   });
 
