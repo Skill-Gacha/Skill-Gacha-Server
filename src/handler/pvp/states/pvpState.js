7597574237c064acb312212237165d18ae75e931
Ayumudayo <yumemiayumu@gmail.com>
2024-12-06 00:50:58 +0900

    🩹 fix: `changeState`도 async로 수정

--- src/handler/pvp/states/pvpState.js
+++ src/handler/pvp/states/pvpState.js
@@ -13,12 +13,12 @@ export default class PvpState extends GameState {
   // changeState(StateClass, options = {})
   // 위와 같은 형태로 여러 옵션 넘겨주기 가능
   // 지금 당장은 필요없으므로 하던대로 사용
-  changeState(StateClass, switchTurn = false) {
+  async changeState(StateClass, switchTurn = false) {
     if (switchTurn) {
       // 구조 분해 할당으로 스왑
       [this.mover, this.stopper] = [this.stopper, this.mover];
     }
     this.pvpRoom.currentState = new StateClass(this.pvpRoom, this.mover, this.stopper);
-    this.pvpRoom.currentState.enter();
+    await this.pvpRoom.currentState.enter();
   }
 }
