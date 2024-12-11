2869d638812da5a014aed04d3ae60aaa58b3ea0a
Dam2204 <soyoung4576@gmail.com>
2024-12-11 15:48:02 +0900

    🩹 fix: 보스전 클리어 시, delay 대신 timer 사용하도록 변경

--- src/handler/boss/states/result/bossMonsterDeadState.js
+++ src/handler/boss/states/result/bossMonsterDeadState.js
@@ -1,17 +1,27 @@
 // src/handler/boss/states/result/bossMonsterDeadState.js
 
-import { BOSS_STATUS } from '../../../../constants/battle.js';
+import { BOSS_GAME_OVER_CONFIRM_TIMEOUT_LIMIT, BOSS_STATUS } from '../../../../constants/battle.js';
 import BossRoomState from '../base/bossRoomState.js';
 import { PacketType } from '../../../../constants/header.js';
 import { createResponse } from '../../../../utils/response/createResponse.js';
 import BossGameOverWinState from './bossGameOverWinState.js';
-import { delay } from '../../../../utils/delay.js';
+import serviceLocator from '#locator/serviceLocator.js';
+import TimerManager from '#managers/timerManager.js';
 
 const DEATH_ANIMATION_CODE = 4;
-const DEFEAT_DELAY = 4000;
 const BOSS_INDEX = 0;
+const RESPONSE_CODE = {
+  SCREEN_TEXT_DONE: 1,
+};
+const BUTTON_CONFIRM = [{ msg: '귀환 대기 중', enable: false }];
 
 export default class BossMonsterDeadState extends BossRoomState {
+  constructor(...args) {
+    super(...args);
+    this.timeoutId = null; // 타이머 식별자 초기화
+    this.timerMgr = serviceLocator.get(TimerManager);
+  }
+
   async enter() {
     this.bossRoom.bossStatus = BOSS_STATUS.MONSTER_DEAD;
     const boss = this.bossRoom.monsters[BOSS_INDEX];
@@ -29,11 +39,29 @@ export default class BossMonsterDeadState extends BossRoomState {
       );
     });
 
-    await delay(DEFEAT_DELAY);
+    // 파티 전멸 로직 전달
+    const partyWinBattleLogResponse = createResponse(PacketType.S_BossBattleLog, {
+      battleLog: {
+        msg: `Null Dragon이 힘을 잃고 쓰러집니다.`,
+        typingAnimation: false,
+        btns: BUTTON_CONFIRM,
+      },
+    });
+    this.users.forEach((user) => {
+      user.socket.write(partyWinBattleLogResponse);
+    });
 
-    this.changeState(BossGameOverWinState);
+    // 타이머 매니저를 통해 타이머 설정
+    this.timeoutId = this.timerMgr.requestTimer(BOSS_GAME_OVER_CONFIRM_TIMEOUT_LIMIT, () => {
+      this.handleInput(RESPONSE_CODE.SCREEN_TEXT_DONE);
+    });
   }
 
   async handleInput(responseCode) {
+    if (responseCode === RESPONSE_CODE.SCREEN_TEXT_DONE) {
+      this.changeState(BossGameOverWinState);
+    } else {
+      invalidResponseCode(this.socket);
+    }
   }
 }
