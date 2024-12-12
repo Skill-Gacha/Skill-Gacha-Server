09f3467f7beae7520d1da970872dd473307f36ff
Ayumudayo <yumemiayumu@gmail.com>
2024-12-12 21:19:26 +0900

    ♻️ refactor: 유틸 계통 코드 일부 정리

--- src/utils/battle/pvpHelpers.js
+++ src/utils/battle/pvpHelpers.js
@@ -3,22 +3,22 @@
 import { BUTTON_OPTIONS } from '../../constants/pvp.js';
 import logger from '../log/logger.js';
 
+// 전투 로그 문자열 생성
 export function generateBattleLog(nickname, suffix, isFirstAttack, turnText) {
   return `${nickname}${suffix} 싸워 이기세요!\n${turnText}`;
 }
 
+// 전투 로그 응답 생성
 export function createBattleLogResponse(msg, isFirstAttack) {
   return {
     msg,
     typingAnimation: false,
-    btns: BUTTON_OPTIONS.map((btn) => ({
-      msg: btn,
-      enable: isFirstAttack
-    }))
+    btns: BUTTON_OPTIONS.map((btn) => ({ msg: btn, enable: isFirstAttack })),
   };
 }
 
+// 잘못된 응답 로깅
 export function sendInvalidResponseLog(socket) {
   logger.error('잘못된 응답 코드가 전달되었습니다.');
-  // 필요 시 socket에 에러 패킷 전송 가능
+  // 필요하다면 socket에 에러 패킷 전송 가능
 }
