343906fcc23bd9883ee0359e21e5abfec37f76f1
Ayumudayo <yumemiayumu@gmail.com>
2024-12-15 14:44:52 +0900

    🩹 fix: Import 누락 수정
    - 네이밍 수정
    - 출력 메세지 포맷 수정

--- src/handler/boss/states/result/bossGameOverWinState.js
+++ src/handler/boss/states/result/bossGameOverWinState.js
@@ -3,9 +3,10 @@
 import { BOSS_STATUS } from '../../../../constants/battle.js';
 import BossRoomState from '../base/bossRoomState.js';
 import { sendBossScreenText } from '../../../../utils/battle/bossHelpers.js';
+import { updateUserResource } from '../../../../db/user/userDb.js';
 
-const gold = 1000000;
-const stone = 1000;
+const REWARD_GOLD_AMOUNT = 1000000;
+const REWARD_STONE_AMOUNT = 1000;
 
 export default class BossGameOverWinState extends BossRoomState {
   async enter() {
@@ -15,14 +16,14 @@ export default class BossGameOverWinState extends BossRoomState {
     // 보상 추가
     await Promise.all(
       this.users.map(async (user) => {
-        user.increaseResource(gold, stone);
+        user.increaseResource(REWARD_GOLD_AMOUNT, REWARD_STONE_AMOUNT);
         await updateUserResource(user.nickname, user.gold, user.stone);
       }),
     );
 
     sendBossScreenText(
       this.users,
-      '축하합니다. Null Dragon을 무찌르는데 성공하셨습니다. \n 1,000,000골드와 강화석 1,000개를 획득하셨습니다.',
+      `축하합니다. Null Dragon을 무찌르는데 성공하셨습니다. \n ${REWARD_GOLD_AMOUNT.toLocaleString('ko-KR')}골드와 강화석 ${REWARD_GOLD_AMOUNT.toLocaleString('ko-KR')}개를 획득 하셨습니다.`,
     );
   }
 
